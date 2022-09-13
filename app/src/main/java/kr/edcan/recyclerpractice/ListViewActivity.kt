e0a65d84c239b97ac0b8ab401c6c9aa86ff9a1e0
Heechan <ckstmznf@naver.com>
2022-09-13 17:38:08 +0900

    ListViewActivity answer

--- app/src/main/java/kr/edcan/recyclerpractice/ListViewActivity.kt
+++ app/src/main/java/kr/edcan/recyclerpractice/ListViewActivity.kt
@@ -2,6 +2,7 @@ package kr.edcan.recyclerpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
 
@@ -11,7 +12,7 @@ class ListViewActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_list_view)
 
         list = findViewById(R.id.list_listView)
         addBtn = findViewById(R.id.btn_listView_addItem)
@@ -19,12 +20,16 @@ class ListViewActivity : AppCompatActivity() {
         val dataList = mutableListOf<String>("Item 1", "Item 2", "Item 3", "Item 4", "Item 5")
         
         //TODO 1. 어댑터 만들기
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_activated_1, dataList)
         
         //TODO 2. 어댑터와 연결 해주기
+        list.adapter = adapter
         
         
         addBtn.setOnClickListener {
-            // TODO 3. 버튼 클릭할때 아이템 추가하기  
+            // TODO 3. 버튼 클릭할때 아이템 추가하기
+            dataList.add("Item ${dataList.size + 1}")
+            adapter.notifyDataSetChanged()
         }
     }
 }
